fccb2dd095a4fdc2e27287d94562387fa2070a2d
kapangyu-gsa <kapang.yu@gsa.gov>
2020-12-18 11:00:07 -0500

    First commit: 12-18-2020

--- topicsanalyser_ui_app.py
+++ topicsanalyser_ui_app.py
@@ -1,30 +1,25 @@
-from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QLineEdit, QSpinBox, QFileDialog, QErrorMessage, QProgressBar
-from PyQt5.QtCore import QRegExp, QBasicTimer
-from PyQt5.QtGui import QRegExpValidator
 import sys
+from PyQt5.QtCore import QRegExp
+from PyQt5.QtGui import QRegExpValidator
 from textfilereader import TextFileReader
 from topicsanalyser import TopicsAnalyser
+from main_form import Ui_MainWindow
+from PyQt5.QtWidgets import (
+    QMainWindow,
+    QApplication, 
+    QFileDialog, 
+    QErrorMessage
+)
 
 class TopicsAnalyser_UI(QMainWindow):
     def __init__(self):
         super(TopicsAnalyser_UI, self).__init__()
-        uic.loadUi('main_form.ui', self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
         
-        self.run_btn = self.findChild(QPushButton, 'run_btn')
-        self.run_btn.clicked.connect(self.run_topics_analyser)
-
-        self.browse_btn = self.findChild(QPushButton, 'browse_btn')
-        self.browse_btn.clicked.connect(self.getfile)
-       
-        self.data_file_txt = self.findChild(QLineEdit, 'data_file_txt')
-        self.num_topics_spb = self.findChild(QSpinBox, 'num_topics_spb')
-        self.num_ngrams_spb = self.findChild(QSpinBox, 'num_ngrams_spb')
-        self.groupby_cols_txt = self.findChild(QLineEdit, 'groupby_cols_txt')
-        self.addl_stopwords_txt = self.findChild(QLineEdit, 'addl_stopwords_txt')
-        
+        self.ui.run_btn.clicked.connect(self.run_topics_analyser)
+        self.ui.browse_btn.clicked.connect(self.getfile)        
         self.setup_validators()
-
         self.show()
         
     def run_topics_analyser(self):        
@@ -32,31 +27,31 @@ class TopicsAnalyser_UI(QMainWindow):
         if (status != 0):
             return
 
-        groupby_cols_text = self.groupby_cols_txt.text()
-        groupby_cols = [col.strip() for col in groupby_cols_text.split(',')]  if (len(groupby_cols_text) > 0) else []
-
-        addl_stopwords_text = self.addl_stopwords_txt.text()
-        addl_stopwords = [sw.strip() for sw in addl_stopwords_text.split(',')] if (len(addl_stopwords_text) > 0) else []
-
+        def get_wordlist(string_: str):
+            return [word.strip() for word in string_.split(',')] if (len(string_) > 0) else []
+        
+        groupby_cols = get_wordlist(self.ui.groupby_cols_txt.text())
+        addl_stopwords = get_wordlist(self.ui.addl_stopwords_txt.text())
+        
         # TODO: these column names should come from the input screen
         text_col = 'Reason for filling position(s) with Federal Government Employee -OTHER'
+        # text_col = 'Please briefly describe an example of one burdensome administrative task or process which you believe is "low value"'
         other_cols = ['AGENCY','COMPONENT','SUB_COMPONENT','GRADELEVEL','SUP_STATUS']
-        data = TextFileReader.get_dataframe(self.data_file_txt.text(), text_col, other_cols)
+        data = TextFileReader.get_dataframe(self.ui.data_file_txt.text(), text_col, other_cols)
         
         analyser = TopicsAnalyser(data)
-        message = analyser.get_topics(self.num_topics_spb.value(), groupby_cols, self.num_ngrams_spb.value(), addl_stopwords)
-        self.statusBar().showMessage(message)
+        message = analyser.get_topics(self.ui.num_topics_spb.value(), groupby_cols, self.ui.num_ngrams_spb.value(), addl_stopwords)
+        self.ui.statusbar.showMessage(message)
 
     def setup_validators(self):
-        # TODO: add field validations
-        # check if the text is an empty string
+       # check if the text is an empty string
         rx = QRegExp('^(?!\\s*$).+')
         self.filename_validator = QRegExpValidator(rx)
 
     def validate_inputs(self):
         self.error_dialog = QErrorMessage()
         errors = []
-        filename_val_status, _, _ = self.filename_validator.validate(self.data_file_txt.text(), 0)
+        filename_val_status, _, _ = self.filename_validator.validate(self.ui.data_file_txt.text(), 0)
         if (filename_val_status != 2):
             errors.append('Data file is required.')
 
@@ -70,7 +65,7 @@ class TopicsAnalyser_UI(QMainWindow):
         options |= QFileDialog.DontUseNativeDialog
         filename, _ = QFileDialog.getOpenFileName(self, "Open File", "", "Excel files (*.xlsx)", options=options) 
         if (filename):
-            self.data_file_txt.setText(filename)        
+            self.ui.data_file_txt.setText(filename)        
 
         
 app = QApplication(sys.argv)
